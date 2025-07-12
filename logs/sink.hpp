87f4727acbabb92d496043ca73cdc929f4d64beb
Your Name <468588126@qq.com>
2025-07-12 16:42:42 +0800

    日志落地模块的实现与简单测试

---
+++ logs/sink.hpp
@@ -0,0 +1,120 @@
+// 日志落地模块
+//   1. 抽象出落地基类
+//   2. 实现不同子类
+//   3. 用简单工厂进行创建与表示的分离
+#pragma once
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+
+#include "util.hpp"
+
+namespace wyllog {
+class LogSink {
+public:
+    using ptr = std::shared_ptr<LogSink>;
+    ~LogSink() {}
+    virtual void log(const char *data, size_t len) = 0;
+};
+
+// 落地方向：标准输出
+class StdoutSink : public LogSink {
+public:
+    using ptr = std::shared_ptr<StdoutSink>;
+    // 将日志消息写到标准输出
+    void log(const char *data, size_t len) { std::cout.write(data, len); }
+};
+
+// 落地方向：指定文件
+class FileSink : public LogSink {
+public:
+    using ptr = std::shared_ptr<FileSink>;
+    // 传入文件名，构造输出流
+    FileSink(const std::string &pathname) : _pathname(pathname) {
+        // 创建指定目录
+        wyllog::file::createDirectory(wyllog::file::path(_pathname));
+        // 打开文件
+        _ofs.open(_pathname, std::ios::binary | std::ios::app);
+        assert(_ofs.is_open());
+    }
+
+    // 将日志消息写到指定文件
+    void log(const char *data, size_t len) {
+        _ofs.write(data, len);
+        assert(_ofs.good());
+    }
+
+private:
+    std::string _pathname;
+    std::ofstream _ofs;
+};
+
+// 落地方向：按照指定文件大小滚动文件
+class RollSinkBySize : public LogSink {
+public:
+    using ptr = std::shared_ptr<RollSinkBySize>;
+    // 传入文件名，和单个文件的上限，构造输出流
+    RollSinkBySize(const std::string &basename, size_t max_size)
+        : _basename(basename),
+          _max_size(max_size),
+          _cur_size(0),
+          _name_count(0) {
+        // 创建指定目录
+        wyllog::file::createDirectory(wyllog::file::path(_basename));
+    }
+    // 将日志消息写到指定文件
+    void log(const char *data, size_t len) {
+        initLogFile();
+        _ofs.write(data, len);
+        assert(_ofs.good());
+        _cur_size += len;
+    }
+
+private:
+    void initLogFile() {
+        if (_ofs.is_open() == false || _cur_size >= _max_size) {
+            _ofs.close();
+            std::string name = createFilename();
+            _ofs.open(name, std::ios::binary | std::ios::app);
+            assert(_ofs.is_open());
+            _cur_size = 0;
+        }
+    }
+
+    // 创建文件
+    std::string createFilename() {
+        time_t time = date::now();
+        struct tm t;
+        localtime_r(&time, &t);
+        std::stringstream ss;
+        ss << _basename;
+        ss << t.tm_year + 1900;
+        ss << t.tm_mon;
+        ss << t.tm_mday;
+        ss << t.tm_hour;
+        ss << t.tm_min;
+        ss << t.tm_sec;
+        ss << "-";
+        ss << _name_count++;
+        ss << ".log";
+        return ss.str();
+    }
+
+private:
+    std::string _basename;  // 基础文件名
+    std::ofstream _ofs;
+    size_t _max_size;  // 单个文件的大小上限
+    size_t _cur_size;  // 当前文件大小
+    size_t _name_count;
+};
+
+class SinkFactory {
+public:
+    template <class Type, class... Args>
+    static LogSink::ptr create(Args &&...args) {
+        return std::make_shared<Type>(std::forward<Args>(args)...);
+    }
+};
+}  // namespace wyllog
--- logs/test.cc
+++ logs/test.cc
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <string>
 
 #include "format.hpp"
 #include "level.hpp"
+#include "sink.hpp"
 #include "util.hpp"
 
 int main() {
@@ -9,6 +11,22 @@ int main() {
                        "一切正常");
     wyllog::Formatter fomatter("xyzxyz%%abc[%d{%H:%M:%S}] %m%n");
     std::string str = fomatter.format(msg);
-    std::cout << str << std::endl;
+    wyllog::LogSink::ptr std_lsp =
+        wyllog::SinkFactory::create<wyllog::StdoutSink>();
+    wyllog::LogSink::ptr file_lsp =
+        wyllog::SinkFactory::create<wyllog::FileSink>("./logfile/test.log");
+    wyllog::LogSink::ptr roll_lsp =
+        wyllog::SinkFactory::create<wyllog::RollSinkBySize>("./logs/roll.log",
+                                                            1024 * 1024);
+    std_lsp->log(str.c_str(), str.size());
+    file_lsp->log(str.c_str(), str.size());
+    size_t cur_size = 0;
+    size_t count = 0;
+    while (cur_size < 1024 * 1024 * 10) {
+        std::string tmp = str + " - " + std::to_string(count++);
+        roll_lsp->log(tmp.c_str(), tmp.size());
+        cur_size += tmp.size();
+    }
+
     return 0;
 }
